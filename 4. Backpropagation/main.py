d21a52dfdadf18a9d5822555305c0ef1910ffc5f
jakubr1712 <jakubr1712@gmail.com>
2023-12-17 18:29:44 +0100

    adding bias

--- 4. Backpropagation/main.py
+++ 4. Backpropagation/main.py
@@ -9,42 +9,55 @@ def sigmoid_derivative(x):
     return x * (1 - x)
 
 
-# Sample input and output data
-X = np.array([[0, 0],
-              [0, 1],
-              [1, 0],
-              [1, 1]])
-y = np.array([[0], [1], [1], [0]])
+# Dane wejściowe i wyjściowe dla problemu XOR
+entrance = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+expected_output = np.array([[0], [1], [1], [0]])
 
+# Inicjalizacja wag i obciążeń
 np.random.seed(1)
+weights_hidden = np.random.uniform(-1, 1, (2, 2))
+bias_hidden = np.random.uniform(-1, 1, (1, 2))
+weights_output = np.random.uniform(-1, 1, (2, 1))
+bias_output = np.random.uniform(-1, 1, (1, 1))
 
-# Random initialization of weights
-weights0 = 2 * np.random.random((2, 4)) - 1
-weights1 = 2 * np.random.random((4, 1)) - 1
 
-# Learning speed parameter
+print("Wagi przed treningiem - warstwa ukryta: \n", weights_hidden)
+print("Obciążenia przed treningiem - warstwa ukryta: \n", bias_hidden)
+print("Wagi przed treningiem - warstwa wyjściowa: \n", weights_output)
+print("Obciążenia przed treningiem - warstwa wyjściowa: \n", bias_output)
+
+
+# Parametry uczenia
 learning_rate = 0.1
+n_epochs = 10000
 
-# The learning process
-for j in range(60000):
-    # forward pass
-    layer0 = X
-    layer1 = sigmoid(np.dot(layer0, weights0))
-    layer2 = sigmoid(np.dot(layer1, weights1))
-    # Error in the output layer
-    layer2_error = y - layer2
+for epoch in range(n_epochs):
+    # Propagacja w przód
+    entrance_input = np.dot(entrance, weights_hidden) + bias_hidden
+    hidden_output = sigmoid(entrance_input)
 
-    if (j % 10000) == 0:
-        print("Error:" + str(np.mean(np.abs(layer2_error))))
+    output_input = np.dot(hidden_output, weights_output) + bias_output
+    predicted_output = sigmoid(output_input)
 
-    # backpropagation
-    layer2_delta = layer2_error * sigmoid_derivative(layer2)
-    layer1_error = layer2_delta.dot(weights1.T)
-    layer1_delta = layer1_error * sigmoid_derivative(layer1)
+    # Obliczanie błędu i propagacja wsteczna
+    error = expected_output - predicted_output
+    delta_output = error * sigmoid_derivative(predicted_output)
 
-    # Weight update
-    weights1 += learning_rate * layer1.T.dot(layer2_delta)
-    weights0 += learning_rate * layer0.T.dot(layer1_delta)
+    error_hidden = delta_output.dot(weights_output.T)
+    delta_hidden = error_hidden * sigmoid_derivative(hidden_output)
 
-print("After training:")
-print(layer2)
+    # Aktualizacja wag i obciążeń
+    weights_output += hidden_output.T.dot(delta_output) * learning_rate
+    bias_output += np.sum(delta_output, axis=0, keepdims=True) * learning_rate
+
+    weights_hidden += entrance.T.dot(delta_hidden) * learning_rate
+    bias_hidden += np.sum(delta_hidden, axis=0, keepdims=True) * learning_rate
+
+
+print("\nWagi po treningu - warstwa ukryta: \n", weights_hidden)
+print("Obciążenia po treningu - warstwa ukryta: \n", bias_hidden)
+print("Wagi po treningu - warstwa wyjściowa: \n", weights_output)
+print("Obciążenia po treningu - warstwa wyjściowa: \n", bias_output)
+
+print("\nWyjście z sieci neuronowej: \n", end='')
+print(predicted_output)
