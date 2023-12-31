c28c70ad47f3ddd4c80b2f664dde8869e3fa253c
jakubr1712 <jakubr1712@gmail.com>
2023-12-31 16:31:49 +0100

    genetic algorithm

--- 5/main.py
+++ 5/main.py
@@ -21,15 +21,9 @@ def fitness(chromosome, data_points):
 # Selekcja ruletki
 def select(population, fitness_values):
     max_fitness = max(fitness_values)
-    normalized_fitness = [max_fitness - f for f in fitness_values]
-    total_fitness = sum(normalized_fitness)
-    selection_probs = [f / total_fitness for f in normalized_fitness]
-    return random.choices(population, weights=selection_probs, k=len(population))
-# def select(population, fitness_values):
-#     total_fitness = sum(fitness_values)
-#     selection_probs = [1 - (f/total_fitness) for f in fitness_values]
-#     print(selection_probs)
-#     return random.choices(population, weights=selection_probs, k=len(population))
+    selection_probs = [(max_fitness - f + 1) / sum(max_fitness - f + 1 for f in fitness_values) for f in fitness_values]
+    selected=random.choices(population, weights=selection_probs, k=len(population))
+    return selected
 
 # Krzyżowanie
 def crossover(ch1, ch2, crossover_rate):
@@ -45,85 +39,87 @@ def mutate(chromosome, mutation_rate):
 
 # Wizualizacja wyników
 def plot_results(data_points, best_chromosome,start_best_chromosome):
-    a, b, c, d = decode(best_chromosome)
     x_values = [x for x, _ in data_points]
     y_values = [y for _, y in data_points]
-    plt.scatter(x_values, y_values, color='red', label='Dane punkty')  # punkty danych
+    plt.scatter(x_values, y_values, color='red', label='Dane punkty')  
 
     x_min, x_max = min(x_values), max(x_values)
-    x_range = np.linspace(x_min, x_max, 500)  # Użycie 500 punktów dla płynności
+    # Użycie 500 punktów dla płynności
+    x_range = np.linspace(x_min, x_max, 500)  
 
-    plt.plot(x_range, [a*x**3 + b*x**2 + c*x + d for x in x_range], color='green', label='Po algorytmie')  # najlepsze dopasowanie
-
+    # najlepsze PRZED dopasowanie
     a, b, c, d = decode(start_best_chromosome)
-    plt.plot(x_range, [a*x**3 + b*x**2 + c*x + d for x in x_range], color='blue', label='Przed algorytmem')  # najlepsze PRZED dopasowanie
+    plt.plot(x_range, [a*x**3 + b*x**2 + c*x + d for x in x_range], color='blue', label='Przed algorytmem')  
+    
+    # najlepsze dopasowanie
+    a, b, c, d = decode(best_chromosome)
+    plt.plot(x_range, [a*x**3 + b*x**2 + c*x + d for x in x_range], color='green', label='Po algorytmie')  
+
     plt.legend()
     plt.show()
+
+def run_genetic_algorithm(mutation_rate, crossover_rate,population_start):
+    best_fitness = float('inf')
+    stagnation_counter = 0
+    population=population_start
+
+    while True:
+        fitness_values = [fitness(ch, data_points) for ch in population]
+        current_best_fitness = min(fitness_values)
+
+        # Sprawdzenie, czy spełniony jest warunek zakończenia
+        if current_best_fitness < threshold_fitness:
+            print("Znaleziono chromosom spełniający kryteria zakończenia.")
+            break
+
+        if current_best_fitness < best_fitness:
+            best_fitness = current_best_fitness
+            stagnation_counter = 0
+        else:
+            stagnation_counter += 1
+            if stagnation_counter >= stagnation_limit:
+                print("Algorytm zatrzymany z powodu stagnacji.")
+                break
+
+        # Selekcja, krzyżowanie i mutacja
+        population = select(population, fitness_values)
+        new_population = []
+        # for i in range(0, N, 2):
+        #     ch1, ch2 = population[i], population[i+1]
+        #     new_ch1, new_ch2 = crossover(ch1, ch2, crossover_rate)
+        #     new_population.extend([mutate(new_ch1, mutation_rate), mutate(new_ch2, mutation_rate)])
+        while len(new_population) < N:
+            ch1, ch2 = random.sample(population, 2)
+            new_ch1, new_ch2 = crossover(ch1, ch2, crossover_rate)
+            new_population.extend([mutate(new_ch1, mutation_rate), mutate(new_ch2, mutation_rate)])
+        population = new_population
+
+    best_chromosome = min(population, key=lambda ch: fitness(ch, data_points))
+    return best_chromosome, fitness(best_chromosome, data_points)
+
 
 # Dane
 data_points = [(-5, -150), (-4, -77), (-3, -30), (-2, 0), (-1, 10), (0.5, 131/8), (1, 18), (2, 25), (3, 32), (4, 75), (5, 130)]
 
-# Parametry algorytmu
-N = 6  # Rozmiar populacji
+N = 8  # Rozmiar populacji
 chromosome_length = 20  # 4 współczynniki * 5 bitów każdy
-threshold_fitness = 1000  # Próg dla funkcji celu
-mutation_rate = 0.3  # Prawdopodobieństwo mutacji
-crossover_rate = 1
+threshold_fitness = 300  # Próg dla funkcji celu
 stagnation_limit = 1000  # Limit stagnacji
-
 # Inicjalizacja populacji
-population = [[random.randint(0, 1) for _ in range(chromosome_length)] for _ in range(N)]
-# prawie ze idealne
-# population=[[1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0],[1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0],[1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0],[1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0],[1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0],[1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0],[1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0],[1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0],[1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0],[1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, #1, 1, 1, 0],]
-# population=[[1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1]for _ in range(N)]
- 
-best_fitness = float('inf')
-stagnation_counter = 0
-
+population_start = [[random.randint(0, 1) for _ in range(chromosome_length)] for _ in range(N)]
 
 # Początkowy najlepszy chromosom
-start_best_chromosome = min(population, key=lambda ch: fitness(ch, data_points))
+start_best_chromosome = min(population_start, key=lambda ch: fitness(ch, data_points))
 
-# Główna pętla algorytmu - teraz jako pętla while
-while True:
-    fitness_values = [fitness(ch, data_points) for ch in population]
-    current_best_fitness = min(fitness_values)
+print(f"PRZED: najlepszy chromosom: {start_best_chromosome} ({decode(start_best_chromosome)}), funkcja celu: {fitness(start_best_chromosome, data_points)}")
 
-    # Sprawdzenie, czy spełniony jest warunek zakończenia
-    if current_best_fitness < threshold_fitness:
-        print("Znaleziono chromosom spełniający kryteria zakończenia.")
-        break
+# Parametry eksperymentu
+mutation_rates = [0.05]
+crossover_rates = [1]
 
-    if current_best_fitness < best_fitness:
-        best_fitness = current_best_fitness
-        stagnation_counter = 0
-    else:
-        stagnation_counter += 1
-        if stagnation_counter >= stagnation_limit:
-            print("Algorytm zatrzymany z powodu stagnacji.")
-            break
+for mutation_rate in mutation_rates:
+    for crossover_rate in crossover_rates:
+        best_chromosome, best_chromosome_fitness = run_genetic_algorithm(mutation_rate, crossover_rate, population_start)
+        print(f"PO: mutacja: {mutation_rate}, krzyżowanie: {crossover_rate}, najlepszy chromosom: {best_chromosome} ({decode(best_chromosome)})  funkcja celu: {best_chromosome_fitness}")
+        plot_results(data_points, best_chromosome, start_best_chromosome)
 
-    # Selekcja, krzyżowanie i mutacja
-    population = select(population, fitness_values)
-
-    new_population = []
-    # for i in range(0, N, 2):
-    #     ch1, ch2 = population[i], population[i+1]
-    #     new_ch1, new_ch2 = crossover(ch1, ch2, crossover_rate)
-    #     new_population.extend([mutate(new_ch1, mutation_rate), mutate(new_ch2, mutation_rate)])
-    while len(new_population) < N:
-        ch1, ch2 = random.sample(population, 2)  # Losowe wybieranie dwóch chromosomów
-        new_ch1, new_ch2 = crossover(ch1, ch2, crossover_rate)
-        new_population.extend([mutate(new_ch1, mutation_rate), mutate(new_ch2, mutation_rate)])
-    population = new_population
-
-
-# Wypisanie ostatecznego najlepszego rozwiązania
-best_chromosome = min(population, key=lambda ch: fitness(ch, data_points))
-decoded_coeffs = decode(best_chromosome)
-
-print(f"Przed Najlepszy chromosom: {start_best_chromosome} ({decode(start_best_chromosome)}), Fitness: {fitness(start_best_chromosome, data_points)}")
-print(f"Po Najlepszy chromosom: {best_chromosome} ({decoded_coeffs}), Fitness: {fitness(best_chromosome, data_points)}")
-
-# Wizualizacja wyników
-plot_results(data_points, best_chromosome,start_best_chromosome)
