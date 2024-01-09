a1eb7072473c1fa05599568350752b5b21bec551
jakubr1712 <jakubr1712@gmail.com>
2024-01-09 20:40:26 +0100

    Genetic Algorithm

---
+++ 5. Genetic Algorithm/README.md
@@ -0,0 +1,38 @@
+# Genetic Algorithm Optimization
+
+This program implements a genetic algorithm for optimizing a function used to fit a curve to data. The program demonstrates how a genetic algorithm utilizes selection, crossover, and mutation to optimize a solution to a problem. Genetic algorithms show effectiveness in optimizing problems where traditional methods may not be efficient. Their flexibility and ability to explore solution spaces make them a powerful tool in various applications.
+
+## Program Overview:
+
+1. Chromosome Decoding:
+   Chromosomes represent potential problem solutions and are encoded as binary sequences. The `decode` function transforms these sequences into function coefficient values.
+
+2. Fitness Function:
+   Used to assess the quality of a chromosome. It is the sum of squared differences between the predicted values by the function and the actual data values.
+
+3. Input and Output Data:
+   The program uses data representing different test cases. Input data consists of combinations of points, and output data consists of expected fits.
+
+4. Population Initialization:
+   The initial population of chromosomes is generated randomly, where each chromosome represents a potential solution.
+
+5. Algorithm Parameters:
+
+   - `mutation_rate`: Determines the probability of chromosome mutation.
+   - `crossover_rate`: Determines the probability of crossover between two chromosomes.
+   - `N`: Number of chromosomes in the population.
+   - `stagnation_limit`: Maximum number of iterations without improvement, after which the algorithm is halted.
+
+6. Roulette Wheel Selection:
+   Selects chromosomes for reproduction based on their fitness. Chromosomes with better fitness have higher chances of being chosen.
+
+7. Crossover and Mutation:
+
+   - Crossover involves merging segments of two chromosomes to create new chromosomes.
+   - Mutation introduces random changes in a chromosome, increasing the genetic diversity of the population.
+
+8. Learning Process:
+   The algorithm iteratively improves the population through selection, crossover, and mutation, aiming to find the chromosome with the lowest fitness value.
+
+9. Results Visualization:
+   The program displays results graphically, showing the fitting of the function to the data before and after applying the genetic algorithm.
---
+++ 5. Genetic Algorithm/main.py
@@ -0,0 +1,127 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Dekodowanie chromosomu
+def decode(chromosome):
+    coeffs = []
+    for i in range(0, len(chromosome), 5):
+        coeff_bits = chromosome[i:i+5]
+        coeff = int(''.join(map(str, coeff_bits[1:])), 2)
+        if coeff_bits[0] == 0:  # sprawdzenie znaku
+            coeff *= -1
+        coeffs.append(coeff)
+    return coeffs
+
+# Funkcja celu
+def fitness(chromosome, data_points):
+    a, b, c, d = decode(chromosome)
+    return sum((a*x**3 + b*x**2 + c*x + d - y)**2 for x, y in data_points)
+ 
+# Selekcja ruletki
+def select(population, fitness_values):
+    max_fitness = max(fitness_values)
+    selection_probs = [(max_fitness - f + 1) / sum(max_fitness - f + 1 for f in fitness_values) for f in fitness_values]
+    selected=random.choices(population, weights=selection_probs, k=len(population))
+    return selected
+
+# Krzyżowanie
+def crossover(ch1, ch2, crossover_rate):
+    if random.random() < crossover_rate:
+        point = random.randint(1, len(ch1) - 2)
+        return ch1[:point] + ch2[point:], ch2[:point] + ch1[point:]
+    else:
+        return ch1, ch2
+
+# Mutacja
+def mutate(chromosome, mutation_rate):
+    return [gene if random.random() > mutation_rate else 1-gene for gene in chromosome]
+
+# Wizualizacja wyników
+def plot_results(data_points, best_chromosome,start_best_chromosome):
+    x_values = [x for x, _ in data_points]
+    y_values = [y for _, y in data_points]
+    plt.scatter(x_values, y_values, color='red', label='Dane punkty')  
+
+    x_min, x_max = min(x_values), max(x_values)
+    # Użycie 500 punktów dla płynności
+    x_range = np.linspace(x_min, x_max, 500)  
+
+    # najlepsze PRZED dopasowanie
+    a, b, c, d = decode(start_best_chromosome)
+    plt.plot(x_range, [a*x**3 + b*x**2 + c*x + d for x in x_range], color='blue', label='Przed algorytmem')  
+    
+    # najlepsze dopasowanie
+    a, b, c, d = decode(best_chromosome)
+    plt.plot(x_range, [a*x**3 + b*x**2 + c*x + d for x in x_range], color='green', label='Po algorytmie')  
+
+    plt.legend()
+    plt.show()
+
+def run_genetic_algorithm(mutation_rate, crossover_rate, population_start):
+    best_fitness = float('inf')
+    stagnation_counter = 0
+    population=population_start
+
+    while True:
+        fitness_values = [fitness(ch, data_points) for ch in population]
+        current_best_fitness = min(fitness_values)
+
+        # Sprawdzenie, czy spełniony jest warunek zakończenia
+        if current_best_fitness < threshold_fitness:
+            print("Znaleziono chromosom spełniający kryteria zakończenia.")
+            break
+
+        if current_best_fitness < best_fitness:
+            best_fitness = current_best_fitness
+            stagnation_counter = 0
+        else:
+            stagnation_counter += 1
+            if stagnation_counter >= stagnation_limit:
+                print("Algorytm zatrzymany z powodu stagnacji.")
+                break
+
+        # Selekcja, krzyżowanie i mutacja
+        population = select(population, fitness_values)
+        new_population = []
+        # for i in range(0, N, 2):
+        #     ch1, ch2 = population[i], population[i+1]
+        #     new_ch1, new_ch2 = crossover(ch1, ch2, crossover_rate)
+        #     new_population.extend([mutate(new_ch1, mutation_rate), mutate(new_ch2, mutation_rate)])
+        while len(new_population) < N:
+            ch1, ch2 = random.sample(population, 2)
+            new_ch1, new_ch2 = crossover(ch1, ch2, crossover_rate)
+            new_population.extend([mutate(new_ch1, mutation_rate), mutate(new_ch2, mutation_rate)])
+        population = new_population
+
+    best_chromosome = min(population, key=lambda ch: fitness(ch, data_points))
+    return best_chromosome, fitness(best_chromosome, data_points)
+
+
+# Dane
+data_points = [(-5, -150), (-4, -77), (-3, -30), (-2, 0), (-1, 10), (0.5, 131/8), (1, 18), (2, 25), (3, 32), (4, 75), (5, 130)]
+
+N = 8  # Rozmiar populacji
+chromosome_length = 20  # 4 współczynniki * 5 bitów każdy
+threshold_fitness = 300  # Próg dla funkcji celu
+stagnation_limit = 1000  # Limit stagnacji
+# Inicjalizacja populacji
+population_start = [[random.randint(0, 1) for _ in range(chromosome_length)] for _ in range(N)]
+
+# Początkowy najlepszy chromosom
+start_best_chromosome = min(population_start, key=lambda ch: fitness(ch, data_points))
+
+print(f"PRZED: najlepszy chromosom: {start_best_chromosome} ({decode(start_best_chromosome)}), funkcja celu: {fitness(start_best_chromosome, data_points)}")
+
+# Parametry eksperymentu
+mutation_rates = [0.05]
+crossover_rates = [1]
+
+for mutation_rate in mutation_rates:
+    for crossover_rate in crossover_rates:
+        best_chromosome, best_chromosome_fitness = run_genetic_algorithm(mutation_rate, crossover_rate, population_start)
+        print(f"PO: mutacja: {mutation_rate}, krzyżowanie: {crossover_rate}, najlepszy chromosom: {best_chromosome} ({decode(best_chromosome)})  funkcja celu: {best_chromosome_fitness}")
+        plot_results(data_points, best_chromosome, start_best_chromosome)
+
+
+input("Naciśnij Enter, aby zakończyć program.")
